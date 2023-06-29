8a8147e4d904845cb88b99d4ff7477f162315e6e
Rahul Kumar <83566650+kumarrah2002@users.noreply.github.com>
2023-06-28 21:08:43 -0400

    added yelp

--- test.py
+++ test.py
@@ -1,24 +1,43 @@
 import requests
 
-# Define the API endpoint URL
-url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+# Define the Google Maps API endpoint URL
+nearbysearch_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
 api_key = 'AIzaSyAhZzoiTUSBFlupc4G0fTROLZBJxaBH9F4'
+
+# Define the Yelp API endpoint URL
+yelp_endpoint = "https://api.yelp.com/v3/businesses/search"
+yelp_headers = dict(
+    Authorization="Bearer 8V0wD0XaZNVI7vNZ4wBoDyWs_CR7jUemUzrGjlYfB6vnquwXf2fvTKH9-lW-s9F6viimgNrbF8hR-VQlt-f3ZL1cIRvkXfDKftN04GxUOv40TDqjFjiouQOnkjo8ZHYx"
+)
+
+miles = 10
+meters = miles * 1609.34
+location = (41.7997986, -72.8259854)
+
 # Set the parameters for the nearby search
-parameters = {
-    "location": "41.7997986,-72.8259854",  # Coordinates
-    "radius": 5000,  # Search radius in meters
+googlemaps_parameters = {
+    "location": "41.7997986, -72.8259854",  # Coordinates
+    "radius": meters,  # Search radius in meters
     # Type of place to search for (e.g., restaurant, cafe, etc.)
     "type": "restaurant",
     "key": api_key  # Replace with your own API key
 }
+#Get Yelp Parameters
+yelp_parameters = {
+    "location": location,
+    "categories": "food,restaurants",
+    "limit": 50,  # limit to the top 50 results
+    "radius": meters
+}
 
 # Send the GET request
-response = requests.get(url, params=parameters)
+google_response = requests.get(nearbysearch_url, params=googlemaps_parameters)
+yelp_response = requests.get(yelp_endpoint, headers=yelp_headers, params=yelp_parameters)
 
 # Check if the request was successful (status code 200)
-if response.status_code == 200:
+if google_response.status_code == 200 and yelp_response.status_code == 200:
     # Get the response data in JSON format
-    data = response.json()
+    data = google_response.json()
 
     # Process the response data
     if data["status"] == "OK":
@@ -55,11 +74,13 @@ if response.status_code == 200:
             if price == 'N/A':
                 print(f"Price: N/A")
             print(f"Categories: {categories}")
-            if open_or_closed == True:
+            if open_or_closed:
                 print("Open")
-            elif open_or_closed == False:
+            elif not open_or_closed:
                 print("Closed")
             print("-----")
+else:
+    print("Error")
         # print(places[3])
 # print(json_data['candidates'][0]['geometry']['location'])
 # print(json_data['candidates'][0]['name'])
